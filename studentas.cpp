3c1afd59a715d437856917be06988aacdad19f03
gabijado <gabija.dociute@gmail.com>
2025-11-07 20:14:46 +0200

    Update studentas.cpp

--- studentas.cpp
+++ studentas.cpp
@@ -1,24 +1,80 @@
 #include "Studentas.h"
-#include <sstream>   // <- ČIA būtina
+#include <sstream>
 #include <algorithm>
 #include <numeric>
+#include <iostream>
+
 using namespace std;
 
+Studentas::Studentas() = default;
+
 Studentas::Studentas(string v, string p, vector<int> nd, int egz)
-    : vardas_(v), pavarde_(p), nd_(nd), egz_(egz) {
+    : vardas_(std::move(v)), pavarde_(std::move(p)), nd_(std::move(nd)), egz_(egz) {
     skaiciuotiGalutinius();
+}
+
+Studentas::Studentas(const Studentas& other)
+    : vardas_(other.vardas_), pavarde_(other.pavarde_),
+    nd_(other.nd_), egz_(other.egz_),
+    galutinisVid_(other.galutinisVid_), galutinisMed_(other.galutinisMed_) {
+}
+
+Studentas::Studentas(Studentas&& other) noexcept
+    : vardas_(std::move(other.vardas_)), pavarde_(std::move(other.pavarde_)),
+    nd_(std::move(other.nd_)), egz_(other.egz_),
+    galutinisVid_(other.galutinisVid_), galutinisMed_(other.galutinisMed_) {
+    other.egz_ = 0;
+    other.galutinisVid_ = other.galutinisMed_ = 0.0;
+}
+
+Studentas& Studentas::operator=(const Studentas& other) {
+    if (this != &other) {
+        vardas_ = other.vardas_;
+        pavarde_ = other.pavarde_;
+        nd_ = other.nd_;
+        egz_ = other.egz_;
+        galutinisVid_ = other.galutinisVid_;
+        galutinisMed_ = other.galutinisMed_;
+    }
+    return *this;
+}
+
+Studentas& Studentas::operator=(Studentas&& other) noexcept {
+    if (this != &other) {
+        vardas_ = std::move(other.vardas_);
+        pavarde_ = std::move(other.pavarde_);
+        nd_ = std::move(other.nd_);
+        egz_ = other.egz_;
+        galutinisVid_ = other.galutinisVid_;
+        galutinisMed_ = other.galutinisMed_;
+        other.egz_ = 0;
+        other.galutinisVid_ = other.galutinisMed_ = 0.0;
+    }
+    return *this;
+}
+
+// --- Destruktorius ---
+Studentas::~Studentas() {
+    
 }
 
 void Studentas::skaiciuotiGalutinius() {
     if (nd_.empty()) {
-        galutinisVid_ = 0.6 * egz_;
-        galutinisMed_ = 0.6 * egz_;
+        galutinisVid_ = galutinisMed_ = 0.6 * egz_;
         return;
     }
+
     double vid = accumulate(nd_.begin(), nd_.end(), 0.0) / nd_.size();
     vector<int> copy_nd = nd_;
     sort(copy_nd.begin(), copy_nd.end());
-    double med = copy_nd[copy_nd.size() / 2];
+
+    double med;
+    size_t size = copy_nd.size();
+    if (size % 2 == 0)
+        med = (copy_nd[size / 2 - 1] + copy_nd[size / 2]) / 2.0;
+    else
+        med = copy_nd[size / 2];
+
     galutinisVid_ = 0.4 * vid + 0.6 * egz_;
     galutinisMed_ = 0.4 * med + 0.6 * egz_;
 }
@@ -34,6 +90,10 @@ istream& Studentas::readStudent(istream& is) {
         egz_ = nd_.back();
         nd_.pop_back();
     }
+    else {
+        egz_ = 0;
+    }
+
     skaiciuotiGalutinius();
     return is;
 }
