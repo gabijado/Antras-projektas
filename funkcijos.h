3cc2895a40e5262bcf9f13f85f67e87abf8bf7f6
gabijado <gabija.dociute@gmail.com>
2025-11-07 11:18:10 +0200

    Refactor funkcijos.h by cleaning up functions
    Removed unused functions and updated include statement.

--- funkcijos.h
+++ funkcijos.h
@@ -1,27 +1,18 @@
 #ifndef FUNKCIJOS_H
 #define FUNKCIJOS_H
 
-#include "studentas.h"
+#include "Studentas.h"
 #include <vector>
 #include <list>
 #include <string>
 
-// Mediana
-double mediana(std::vector<int> paz);
+void generuotiFaila(int kiekis);
 
-// Skaityti is failo 
 template <typename Container>
 Container skaitytiIsFailo(const std::string& failo_pav);
 
-// Rodyti rezultatus
 void rodytiRezultatus(std::vector<Studentas>& studentai);
 
-// Skirstyti ir rasyti i failus
-template <typename Container>
-void skirstytiIrRasyti(const Container& studentai, const std::string& konteinerio_pav,
-    double& skirstymo_laikas, double& rasymo_laikas);
-
-// Naujos strategijos 
 template <typename Container>
 void skirstytiStrategija1(const Container& studentai, const std::string& konteinerio_pav,
     double& skirstymo_laikas, double& rasymo_laikas);
@@ -34,10 +25,6 @@ template <typename Container>
 void skirstytiStrategija3(Container& studentai, const std::string& konteinerio_pav,
     double& skirstymo_laikas, double& rasymo_laikas);
 
-// Generuoti testini faila
-void generuotiFaila(int kiekis);
-
-// Irasyti rezultatus i readme
 void irasytiRezultatusReadme(double v_read, double v_split, double v_write,
     double l_read, double l_split, double l_write,
     const std::string& failo_pav);
