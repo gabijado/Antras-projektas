6255a4455737971a0f63de499aa3e19a53d05ad2
gabijado <gabija.dociute@gmail.com>
2025-11-07 12:01:11 +0200

    Refactor Studentas class and update member variables
    Refactor Studentas class to use default constructor and updated member variables.

--- studentas.h
+++ studentas.h
@@ -1,41 +1,28 @@
-#ifndef STUDENTAS_H
-#define STUDENTAS_H
-
+#pragma once
 #include <iostream>
 #include <vector>
 #include <string>
+#include <numeric>
 #include <algorithm>
-#include <sstream>
 
 class Studentas {
 private:
     std::string vardas_;
     std::string pavarde_;
     std::vector<int> nd_;
-    int egz_;
-    double gal_vid_;
-    double gal_med_;
+    int egz_ = 0;
+    double galutinisVid_ = 0.0;
+    double galutinisMed_ = 0.0;
 
 public:
-    Studentas() : vardas_(""), pavarde_(""), egz_(0), gal_vid_(0.0), gal_med_(0.0) {}
-    Studentas(const std::string& vardas, const std::string& pavarde,
-        const std::vector<int>& nd, int egz)
-        : vardas_(vardas), pavarde_(pavarde), nd_(nd), egz_(egz) {
-        skaiciuotiGalutinius();
-    }
+    Studentas() = default;
+    Studentas(std::string v, std::string p, std::vector<int> nd, int egz);
 
-    // get'eriai
     std::string vardas() const { return vardas_; }
     std::string pavarde() const { return pavarde_; }
-    double galutinisVid() const { return gal_vid_; }
-    double galutinisMed() const { return gal_med_; }
-    int egzaminas() const { return egz_; }
+    double galutinisVid() const { return galutinisVid_; }
+    double galutinisMed() const { return galutinisMed_; }
 
-    std::istream& readStudent(std::istream& is);
-
-    double skaiciuotiVidurki() const;
-    double skaiciuotiMediana() const;
     void skaiciuotiGalutinius();
+    std::istream& readStudent(std::istream& is);
 };
-
-#endif
