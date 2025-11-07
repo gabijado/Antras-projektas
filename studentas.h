48be1476effffa2e4fb36c864c08dc744333cff3
gabijado <gabija.dociute@gmail.com>
2025-11-07 20:14:11 +0200

    Update studentas.h

--- studentas.h
+++ studentas.h
@@ -1,7 +1,9 @@
-#pragma once
-#include <iostream>
-#include <vector>
+#ifndef STUDENTAS_H
+#define STUDENTAS_H
+
 #include <string>
+#include <vector>
+#include <iostream>
 #include <numeric>
 #include <algorithm>
 
@@ -15,14 +17,28 @@ private:
     double galutinisMed_ = 0.0;
 
 public:
-    Studentas() = default;
+    // --- Konstruktoriai ---
+    Studentas(); 
     Studentas(std::string v, std::string p, std::vector<int> nd, int egz);
 
+    
+    Studentas(const Studentas& other);
+    Studentas(Studentas&& other) noexcept;
+    Studentas& operator=(const Studentas& other);
+    Studentas& operator=(Studentas&& other) noexcept;
+
+    // --- Destruktorius ---
+    ~Studentas();  // Aprašytas Studentas.cpp faile
+
+    // --- Get'ai ---
     std::string vardas() const { return vardas_; }
     std::string pavarde() const { return pavarde_; }
     double galutinisVid() const { return galutinisVid_; }
     double galutinisMed() const { return galutinisMed_; }
 
+    // --- Pagalbinės funkcijos ---
     void skaiciuotiGalutinius();
     std::istream& readStudent(std::istream& is);
 };
+
+#endif
