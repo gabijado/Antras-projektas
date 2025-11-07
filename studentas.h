34cbd65dfda7373f7e2ae304f5071760f9b904c8
gabijado <gabija.dociute@gmail.com>
2025-11-07 11:17:22 +0200

    Refactor Studentas class and update member variables

--- studentas.h
+++ studentas.h
@@ -2,44 +2,40 @@
 #define STUDENTAS_H
 
 #include <iostream>
-#include <string>
 #include <vector>
-#include "funkcijos.h"
-
-using namespace std;
+#include <string>
+#include <algorithm>
+#include <sstream>
 
 class Studentas {
 private:
-    string vardas_;
-    string pavarde_;
-    vector<int> nd_;
+    std::string vardas_;
+    std::string pavarde_;
+    std::vector<int> nd_;
     int egz_;
     double gal_vid_;
     double gal_med_;
 
 public:
-    Studentas() : egz_(0), gal_vid_(0.0), gal_med_(0.0) {}      
-    Studentas(istream& is);                                  
+    Studentas() : vardas_(""), pavarde_(""), egz_(0), gal_vid_(0.0), gal_med_(0.0) {}
+    Studentas(const std::string& vardas, const std::string& pavarde,
+        const std::vector<int>& nd, int egz)
+        : vardas_(vardas), pavarde_(pavarde), nd_(nd), egz_(egz) {
+        skaiciuotiGalutinius();
+    }
 
     // get'eriai
-    inline string vardas() const { return vardas_; }
-    inline string pavarde() const { return pavarde_; }
-    inline double galVid() const { return gal_vid_; }
-    inline double galMed() const { return gal_med_; }
+    std::string vardas() const { return vardas_; }
+    std::string pavarde() const { return pavarde_; }
+    double galutinisVid() const { return gal_vid_; }
+    double galutinisMed() const { return gal_med_; }
+    int egzaminas() const { return egz_; }
 
+    std::istream& readStudent(std::istream& is);
 
-    istream& readStudent(istream&);
-
-    // skaičiavimo metodai
     double skaiciuotiVidurki() const;
     double skaiciuotiMediana() const;
-
-    // galutinio balo skaičiavimas
     void skaiciuotiGalutinius();
-
-    friend bool compare(const Studentas&, const Studentas&);
-    friend bool comparePagalPavarde(const Studentas&, const Studentas&);
-    friend bool comparePagalEgza(const Studentas&, const Studentas&);
 };
 
 #endif
