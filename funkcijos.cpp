170173d707581d6e657f0c49d05481af6fb44089
gabijado <gabija.dociute@gmail.com>
2025-11-07 12:02:09 +0200

    Update funkcijos.cpp

--- funkcijos.cpp
+++ funkcijos.cpp
@@ -1,10 +1,12 @@
 #include "funkcijos.h"
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <iomanip>
 #include <vector>
 #include <list>
 #include <algorithm>
+#include <numeric>
 #include <chrono>
 #include <cstdlib>
 
@@ -45,7 +47,6 @@ Container skaitytiIsFailo(const string& failo_pav) {
     return studentai;
 }
 
-// template instantiation
 template vector<Studentas> skaitytiIsFailo<vector<Studentas>>(const string&);
 template list<Studentas> skaitytiIsFailo<list<Studentas>>(const string&);
 
@@ -59,39 +60,87 @@ void rodytiRezultatus(vector<Studentas>& studentai) {
     }
 }
 
-// Skirstymo strategijos (paprastai skirsto pagal galutinisVid() < 5)
+// Skirstymo strategija 1
 template <typename Container>
-void skirstytiStrategija1(const Container& studentai, const string& konteinerio_pav,
-    double& skirstymo_laikas, double& rasymo_laikas) {
-    // paprasta kopija ir skirstymas
+void skirstytiStrategija1(Container& studentai, const string&, double& split_time, double& write_time) {
+    auto start_split = high_resolution_clock::now();
+    Container geri, blogi;
+    for (auto& s : studentai) {
+        if (s.galutinisVid() >= 5) geri.push_back(s);
+        else blogi.push_back(s);
+    }
+    auto end_split = high_resolution_clock::now();
+    split_time = duration<double>(end_split - start_split).count();
+
+    auto start_write = high_resolution_clock::now();
+    ofstream out_g("geri.txt"), out_b("blogi.txt");
+    for (auto& s : geri) out_g << s.vardas() << " " << s.pavarde() << " " << s.galutinisVid() << "\n";
+    for (auto& s : blogi) out_b << s.vardas() << " " << s.pavarde() << " " << s.galutinisVid() << "\n";
+    out_g.close(); out_b.close();
+    auto end_write = high_resolution_clock::now();
+    write_time = duration<double>(end_write - start_write).count();
 }
 
+// Skirstymo strategija 2 (naudojant erase)
 template <typename Container>
-void skirstytiStrategija2(Container& studentai, const string& konteinerio_pav,
-    double& skirstymo_laikas, double& rasymo_laikas) {
-    // optimizuota su erase
+void skirstytiStrategija2(Container& studentai, const string&, double& split_time, double& write_time) {
+    auto start_split = high_resolution_clock::now();
+    Container blogi;
+    auto it = studentai.begin();
+    while (it != studentai.end()) {
+        if (it->galutinisVid() < 5) {
+            blogi.push_back(*it);
+            it = studentai.erase(it);
+        }
+        else it++;
+    }
+    auto end_split = high_resolution_clock::now();
+    split_time = duration<double>(end_split - start_split).count();
+
+    auto start_write = high_resolution_clock::now();
+    ofstream out_g("geri.txt"), out_b("blogi.txt");
+    for (auto& s : studentai) out_g << s.vardas() << " " << s.pavarde() << " " << s.galutinisVid() << "\n";
+    for (auto& s : blogi) out_b << s.vardas() << " " << s.pavarde() << " " << s.galutinisVid() << "\n";
+    write_time = duration<double>(high_resolution_clock::now() - start_write).count();
 }
 
+// Skirstymo strategija 3 (partition)
 template <typename Container>
-void skirstytiStrategija3(Container& studentai, const string& konteinerio_pav,
-    double& skirstymo_laikas, double& rasymo_laikas) {
-    // optimizuota su partition
+void skirstytiStrategija3(Container& studentai, const string&, double& split_time, double& write_time) {
+    auto start_split = high_resolution_clock::now();
+    Container blogi;
+    auto p = partition(studentai.begin(), studentai.end(), [](const Studentas& s) { return s.galutinisVid() >= 5; });
+    blogi.insert(blogi.end(), p, studentai.end());
+    studentai.erase(p, studentai.end());
+    auto end_split = high_resolution_clock::now();
+    split_time = duration<double>(end_split - start_split).count();
+
+    auto start_write = high_resolution_clock::now();
+    ofstream out_g("geri.txt"), out_b("blogi.txt");
+    for (auto& s : studentai) out_g << s.vardas() << " " << s.pavarde() << " " << s.galutinisVid() << "\n";
+    for (auto& s : blogi) out_b << s.vardas() << " " << s.pavarde() << " " << s.galutinisVid() << "\n";
+    write_time = duration<double>(high_resolution_clock::now() - start_write).count();
 }
 
-// template instantiation
-template void skirstytiStrategija1<vector<Studentas>>(const vector<Studentas>&, const string&, double&, double&);
-template void skirstytiStrategija1<list<Studentas>>(const list<Studentas>&, const string&, double&, double&);
-
+template void skirstytiStrategija1<vector<Studentas>>(vector<Studentas>&, const string&, double&, double&);
+template void skirstytiStrategija1<list<Studentas>>(list<Studentas>&, const string&, double&, double&);
 template void skirstytiStrategija2<vector<Studentas>>(vector<Studentas>&, const string&, double&, double&);
 template void skirstytiStrategija2<list<Studentas>>(list<Studentas>&, const string&, double&, double&);
-
 template void skirstytiStrategija3<vector<Studentas>>(vector<Studentas>&, const string&, double&, double&);
 template void skirstytiStrategija3<list<Studentas>>(list<Studentas>&, const string&, double&, double&);
 
 void irasytiRezultatusReadme(double v_read, double v_split, double v_write,
     double l_read, double l_split, double l_write,
     const string& failo_pav) {
+
     ofstream fout("readme.md", ios::app);
-    fout << "\n## Rezultatai (" << failo_pav << ")\n";
+    fout << "\nRezultatai failui: " << failo_pav << "\n";
+    fout << left << setw(15) << "Konteineris"
+        << setw(15) << "Nuskaitymas(s)"
+        << setw(15) << "Skirstymas(s)"
+        << setw(15) << "Rasymas(s)" << "\n";
+    fout << string(60, '-') << "\n";
+    fout << left << setw(15) << "Vector" << setw(15) << v_read << setw(15) << v_split << setw(15) << v_write << "\n";
+    fout << left << setw(15) << "List" << setw(15) << l_read << setw(15) << l_split << setw(15) << l_write << "\n";
     fout.close();
 }
