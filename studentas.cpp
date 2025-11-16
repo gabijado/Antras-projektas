7b84c79d2915e96f1d5dbd22a01c49b3f855d1d3
gabijado <gabija.dociute@gmail.com>
2025-11-16 13:16:32 +0200

    Refactor Studentas class and implement I/O operators
    Refactor Studentas class constructors and operators, remove unused includes and methods, and implement input/output operators.

--- studentas.cpp
+++ studentas.cpp
@@ -1,40 +1,25 @@
 #include "Studentas.h"
+#include <iomanip>
 #include <sstream>
-#include <algorithm>
-#include <numeric>
-#include <iostream>
-
-using namespace std;
 
 // --- Konstruktoriai ---
 Studentas::Studentas()
-    : vardas_(""), pavarde_(""), nd_(), egz_(0), galutinisVid_(0.0), galutinisMed_(0.0) {
+    : vardas_(""), pavarde_(""), egz_(0), galutinisVid_(0.0), galutinisMed_(0.0) {
 }
 
-// Parametrinis konstruktorius
-Studentas::Studentas(const string& v, const string& p, const vector<int>& nd, int egz)
+Studentas::Studentas(const std::string& v, const std::string& p, const std::vector<int>& nd, int egz)
     : vardas_(v), pavarde_(p), nd_(nd), egz_(egz) {
     skaiciuotiGalutinius();
 }
 
-// Kopijavimo konstruktorius
+// --- Kopijavimo konstruktorius ---
 Studentas::Studentas(const Studentas& other)
     : vardas_(other.vardas_), pavarde_(other.pavarde_),
     nd_(other.nd_), egz_(other.egz_),
     galutinisVid_(other.galutinisVid_), galutinisMed_(other.galutinisMed_) {
 }
 
-// Move konstruktorius
-Studentas::Studentas(Studentas&& other)
-    : vardas_(std::move(other.vardas_)), pavarde_(std::move(other.pavarde_)),
-    nd_(std::move(other.nd_)), egz_(other.egz_),
-    galutinisVid_(other.galutinisVid_), galutinisMed_(other.galutinisMed_) {
-    other.egz_ = 0;
-    other.galutinisVid_ = 0.0;
-    other.galutinisMed_ = 0.0;
-}
-
-// Kopijavimo priskyrimo operatorius
+// --- Kopijavimo priskyrimo operatorius ---
 Studentas& Studentas::operator=(const Studentas& other) {
     if (this != &other) {
         vardas_ = other.vardas_;
@@ -43,22 +28,6 @@ Studentas& Studentas::operator=(const Studentas& other) {
         egz_ = other.egz_;
         galutinisVid_ = other.galutinisVid_;
         galutinisMed_ = other.galutinisMed_;
-    }
-    return *this;
-}
-
-// Move priskyrimo operatorius
-Studentas& Studentas::operator=(Studentas&& other) {
-    if (this != &other) {
-        vardas_ = std::move(other.vardas_);
-        pavarde_ = std::move(other.pavarde_);
-        nd_ = std::move(other.nd_);
-        egz_ = other.egz_;
-        galutinisVid_ = other.galutinisVid_;
-        galutinisMed_ = other.galutinisMed_;
-        other.egz_ = 0;
-        other.galutinisVid_ = 0.0;
-        other.galutinisMed_ = 0.0;
     }
     return *this;
 }
@@ -73,6 +42,35 @@ Studentas::~Studentas() {
     galutinisMed_ = 0.0;
 }
 
+// --- Įvesties operatorius ---
+std::istream& operator>>(std::istream& is, Studentas& s) {
+    s.nd_.clear();
+    is >> s.vardas_ >> s.pavarde_;
+
+    int pazymys;
+    while (is >> pazymys) s.nd_.push_back(pazymys);
+
+    if (!s.nd_.empty()) {
+        s.egz_ = s.nd_.back();
+        s.nd_.pop_back();
+    }
+    else {
+        s.egz_ = 0;
+    }
+
+    s.skaiciuotiGalutinius();
+
+    return is;
+}
+
+// --- Išvesties operatorius ---
+std::ostream& operator<<(std::ostream& os, const Studentas& s) {
+    os << std::left << std::setw(15) << s.vardas_ << std::setw(20) << s.pavarde_
+        << std::setw(12) << std::fixed << std::setprecision(2) << s.galutinisVid_
+        << std::setw(12) << std::fixed << std::setprecision(2) << s.galutinisMed_;
+    return os;
+}
+
 // --- Pagalbinės funkcijos ---
 void Studentas::skaiciuotiGalutinius() {
     if (nd_.empty()) {
@@ -80,9 +78,10 @@ void Studentas::skaiciuotiGalutinius() {
         return;
     }
 
-    double vid = accumulate(nd_.begin(), nd_.end(), 0.0) / nd_.size();
-    vector<int> copy_nd = nd_;
-    sort(copy_nd.begin(), copy_nd.end());
+    double vid = std::accumulate(nd_.begin(), nd_.end(), 0.0) / nd_.size();
+
+    std::vector<int> copy_nd = nd_; // kopijuojame, nes sort modifikuoja
+    std::sort(copy_nd.begin(), copy_nd.end());
 
     double med;
     size_t size = copy_nd.size();
@@ -95,20 +94,6 @@ void Studentas::skaiciuotiGalutinius() {
     galutinisMed_ = 0.4 * med + 0.6 * egz_;
 }
 
-istream& Studentas::readStudent(istream& is) {
-    is >> vardas_ >> pavarde_;
-    nd_.clear();
-    int paz;
-    while (is >> paz) {
-        nd_.push_back(paz);
-    }
-    if (!nd_.empty()) {
-        egz_ = nd_.back();
-        nd_.pop_back();
-    }
-    else {
-        egz_ = 0;
-    }
-    skaiciuotiGalutinius();
-    return is;
+std::istream& Studentas::readStudent(std::istream& is) {
+    return is >> *this;
 }
